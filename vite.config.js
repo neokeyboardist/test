5b804eedd7b328d9950cc051baf658c755d253f6
tesla <neokeyboardist@gmail.com>
2023-09-24 13:03:59 +0300

    adding vite-plugin-pwa

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,12 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/test/",
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    VitePWA()
+  ],
 })
